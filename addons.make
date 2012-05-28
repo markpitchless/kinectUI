c104aec62b238657b5e4eda0e880831ee1c721dc
Mark Pitchless <markpitchless@gmail.com>
2012-05-28 14:58:29 +0100

    Add basic app skeleton.

---
+++ addons.make
@@ -0,0 +1,3 @@
+ofxUI
+ofxKinect
+ofxXmlSettings
---
+++ src/kinectUIApp.cpp
@@ -0,0 +1,61 @@
+#include "kinectUIApp.h"
+
+//--------------------------------------------------------------
+void kinectUIApp::setup(){
+
+}
+
+//--------------------------------------------------------------
+void kinectUIApp::update(){
+
+}
+
+//--------------------------------------------------------------
+void kinectUIApp::draw(){
+
+}
+
+//--------------------------------------------------------------
+void kinectUIApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void kinectUIApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void kinectUIApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void kinectUIApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void kinectUIApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void kinectUIApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void kinectUIApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void kinectUIApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void kinectUIApp::dragEvent(ofDragInfo dragInfo){
+
+}
